71c0263d9e08f4ab5fcd83455e823d0c6b4e2ccb
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-12-14 22:58:55 +0900

    1214_10:58

--- src/main/java/com/clova/nlp2/Nlp2Application.java
+++ src/main/java/com/clova/nlp2/Nlp2Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Nlp2Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Nlp2Application.class, args);
 	}
 
 }
+
