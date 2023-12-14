28879af61191a340d5763be1be929ab7b214a082
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-12-14 15:51:12 +0900

    nlp_1214

---
+++ src/test/java/com/clova/nlp2/Nlp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clova.nlp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nlp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
